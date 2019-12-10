9dddd2263c36a918607d1fac5d9875a1bc83d4ef
kawa1228 <sxa.1105bo@gmail.com>
2019-12-10 10:25:37 +0900

    homeとhelloのページを作成しルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static#index'
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
+
+  # RailsからAppコンポーネントにAPI以外のリクエストを送信
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  # root to static controller
+  root 'static#index'
 end
